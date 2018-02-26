cfb4e48a26194a4723da10c26920f46618e1b2bd
uri baum <uribaum@gmail.com>
2018-02-26 23:26:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
+import { FileValidators } from "./file-validators";
+import { VERSION } from "@angular/material";
+
+import "rxjs/add/operator/map";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "material-app",
+  templateUrl: "app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private version: any;
+  formDoc: FormGroup;
+  private ngVersion = VERSION;
+
+  constructor(http: HttpClient, private _fb: FormBuilder) {
+    // Display the currently used Material 2 version.
+    http
+      .get("https://api.github.com/repos/angular/material2-builds/commits/HEAD")
+      .subscribe(d => (this.version = d));
+  }
+
+  ngOnInit() {
+    this.formDoc = this._fb.group({
+      basicfile: [],
+      requiredfile: [
+        { value: undefined, disabled: false },
+        [Validators.required, FileValidators.maxContentSize(104857600)]
+      ],
+      disabledfile: [{ value: undefined, disabled: true }],
+      multiplefile: [{ value: undefined, disabled: false }]
+    });
+  }
+
+  onSubmit() {
+    console.log("SUBMITTED", this.formDoc);
+  }
 }
+
+/*
+  Copyright 2017 Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at http://angular.io/license
+ */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,104 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { CdkTableModule } from "@angular/cdk/table";
+import { OverlayModule } from "@angular/cdk/overlay";
+import { ReactiveFormsModule } from "@angular/forms";
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatSortModule,
+  MatPaginatorModule
+} from "@angular/material";
+import { ByteFormatPipe } from "./byte-format.pipe";
+import { InputFileComponent } from "./input-file/input-file.component";
 
-
-import { AppComponent } from './app.component';
-
+/**
+ * NgModule that includes all Material modules that are required to serve
+ * the Plunker.
+ */
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    // CDk
+    CdkTableModule,
+    OverlayModule,
+
+    // Material
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+
+    MatNativeDateModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  declarations: [AppComponent, InputFileComponent, ByteFormatPipe],
+  bootstrap: [AppComponent],
+  providers: []
 })
-export class AppModule { }
+export class PlunkerAppModule {}
+
+/*
+  Copyright 2017 Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at http://angular.io/license
+ */
